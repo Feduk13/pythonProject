e09cb7541dd27879f955d73abdd6c4522e6535d3
Feduk13 <masterkovalev085@gmail.com>
2023-01-29 18:08:08 +0200

    new version in python

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '_
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint._main__':
-    print_hi('Pyharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class Student:
+    def __init__(self, height=160):
+        self.height = height
+nick = Student()
+kate = Student(height=170)
+print(nick.height)
+print(kate.height)
+print("Доброго дня")
