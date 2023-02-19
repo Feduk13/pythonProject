c4b1d093b3bd233658532448097da63cf9679158
Feduk13 <masterkovalev085@gmail.com>
2023-02-19 15:36:49 +0200

    Hello word

--- lesson 6.py
+++ lesson 6.py
@@ -1,16 +1,21 @@
-result = []
 def divider(a, b):
-    if a < b:
-       raise ValueError
-    if b > 100:
-       raise IndexError
-    return a/b
-data = {10: 2, 8: 0, []: 15, :4}
-for key in data:
     try:
-        res = divider(key, data[key])
-        result.append(res)
-    except Exception as err:
-        print(type(err))
-
+        return a / b
+    except ZeroDivisionError:
+        print("Ты не можешь делить на 0")
+        return 0
+    except TypeError:
+        print("Ты не можешь делить не числовые значения")
+        return 0
+    except ValueError:
+        print("Ты не можешь делить не числовые значения")
+        return 0
+data = {10: 2, 2: 5, "123": 4, 18: 0, 8 : 4}
+result = []
+for key in data:
+    res = divider(key, data[key])
+    result.append(res)
 print(result)
+
+
+
