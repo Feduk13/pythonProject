7aa89280b12f15a59d032474bde98783f3e1db80
Feduk13 <masterkovalev085@gmail.com>
2023-02-19 13:11:29 +0200

    Hello word

--- main.py
+++ main.py
@@ -1,4 +1,53 @@
 import random
+class Cat:
+
+    def __init__(self, name):
+        self.name = name
+        self.gladness = 60
+        self.progress = 5
+        self.alive = True
+    def to_sleep(self):
+        print("may may may")
+        self.progress += 0.20
+        self.gladness += 3
+    def to_chill(self):
+        print("may may")
+        self.gladness += 5
+        self.progress += 1
+    def to_tugudk(self):
+        print("tugudk")
+        self.gladness += 8
+        self.progress -= 0.11
+
+
+    def is_alive(self):
+        if self.progress < -0.5:
+            print("Cast out…")
+            self.alive = False
+        elif self.gladness <= 0:
+            print("Depression…")
+            self.alive = False
+        elif self.progress > 5:
+            print("Passed externally…")
+            self.alive = False
+
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {round(self.progress, 2)}")
+
+    def live2(self, day):
+        day = "Day" + str(day) + "of" + self.name + "life"
+        print(f"{day:=^50}")
+        live_cube = random.randint(1, 3)
+        if live_cube == 1:
+            self.to_tugudk()
+        elif live_cube == 2:
+            self.to_sleep()
+        elif live_cube == 3:
+            self.to_chill()
+        self.end_of_day()
+        self.is_alive()
+
 class Student:
 
     def __init__(self, name):
@@ -51,10 +100,14 @@ class Student:
 
 nick = Student(name="Nick")
 kate = Student(name="Kate")
+Barsik = Cat(name="Barsik")
 for day in range(365):
     if nick.alive == False:
         break
     nick.live(day)
+    if Barsik.alive == False:
+        break
+    Barsik.live2(day)
     if kate.alive == False:
         break
     kate.live(day)
