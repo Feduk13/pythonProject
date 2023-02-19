e43ace545a8d0f310ddca61a09188ef87d0cb6cd
Feduk13 <masterkovalev085@gmail.com>
2023-02-19 12:19:17 +0200

    Hello word

--- lesson 6.py
+++ lesson 6.py
@@ -1,8 +1,16 @@
-try:
-    print("start code")
-    print(error)
-    print("No errors")
-except:
-    print("we have an errors")
+result = []
+def divider(a, b):
+    if a < b:
+       raise ValueError
+    if b > 100:
+       raise IndexError
+    return a/b
+data = {10: 2, 8: 0, []: 15, :4}
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except Exception as err:
+        print(type(err))
 
-print("code after capsule")
+print(result)
