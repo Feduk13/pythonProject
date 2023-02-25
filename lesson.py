e0380336950f3814bd5782418b95e92f5ca50353
Feduk13 <masterkovalev085@gmail.com>
2023-02-25 09:43:19 +0200

    Hello word

---
+++ lesson.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Back, Style
+print(Fore.GREEN + 'ШАГ')
+print(Back.BLACK + 'Hello word ist Fedia')
+
+#  цвет текста
+# Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+#  цвет фона
+# Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+# яркость текста  сброс
+# Style: DIM, NORMAL, BRIGHT, RESET_ALL
---
+++ tets.py
@@ -0,0 +1,4 @@
+import unittest
+from lesson 8 import *
+
+
